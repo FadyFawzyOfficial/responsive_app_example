5c79715ae00132a6fcb3814f2a2ad3a7f8aed304
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-08-30 17:18:06 +0300

    5. Change the Value (of fontSize for Ex.) depending on App Size (Responsive Values)

--- lib/widgets/screen_header.dart
+++ lib/widgets/screen_header.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
 class ScreenHeader extends StatelessWidget {
   const ScreenHeader({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Stack(
+    return Stack(
       alignment: Alignment.center,
       children: [
-        Image(
+        const Image(
           width: 800,
           color: Color.fromRGBO(255, 255, 255, 0.5),
           colorBlendMode: BlendMode.modulate,
@@ -18,7 +19,14 @@ class ScreenHeader extends StatelessWidget {
           'Our Courses',
           textAlign: TextAlign.center,
           style: TextStyle(
-            fontSize: 60,
+            fontSize: ResponsiveValue(
+              context,
+              defaultValue: 60.0,
+              valueWhen: [
+                const Condition.smallerThan(name: MOBILE, value: 40.0),
+                const Condition.largerThan(name: TABLET, value: 80.0),
+              ],
+            ).value,
             color: Colors.blueGrey,
             fontWeight: FontWeight.bold,
           ),
